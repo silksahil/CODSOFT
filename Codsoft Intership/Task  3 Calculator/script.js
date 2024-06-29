a4bf070fca1713ab2358a77deaefb7795df75e5d
Sahil Raut <145009742+silksahil@users.noreply.github.com>
2024-06-29 10:35:02 +0530

    Add files via upload

---
+++ Codsoft Intership/Task  3 Calculator/script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let str = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+                str = eval(str);
+                input.value = str;
+        } else if (e.target.innerHTML == "AC") {
+            str = "";
+            input.value = str;
+        } else if (e.target.innerHTML == "DEL") {
+            str = str.slice(0, -1);
+            input.value = str;
+        } else {
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+    });
+});
